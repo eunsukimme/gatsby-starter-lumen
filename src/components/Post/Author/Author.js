283b19b39159cfeb68f1e5eae2f3ac4ca26035b8
eunsukimme <eunsu.dev@gmail.com>
2020-05-31 22:52:32 +0900

    refactor: add profile icon on footer in post
    also added posts and edited color scheme of codeblock
    *refactoring is not completed.

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,23 +1,29 @@
 // @flow strict
-import React from 'react';
-import { getContactHref } from '../../../utils';
-import styles from './Author.module.scss';
-import { useSiteMetadata } from '../../../hooks';
+import React from "react";
+import { getContactHref } from "../../../utils";
+import styles from "./Author.module.scss";
+import { useSiteMetadata } from "../../../hooks";
+import profileImage from "../../../../static/photo.jpg";
 
 const Author = () => {
   const { author } = useSiteMetadata();
 
   return (
-    <div className={styles['author']}>
-      <p className={styles['author__bio']}>
+    <div className={styles["author"]}>
+      <img
+        src={profileImage}
+        alt={author.name}
+        style={{ width: 75, height: 75, borderRadius: "100%" }}
+      />
+      <p className={styles["author__bio"]}>
         {author.bio}
         <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
+          className={styles["author__bio-twitter"]}
+          href={getContactHref("github", author.contacts.github)}
           rel="noopener noreferrer"
           target="_blank"
         >
-          <strong>{author.name}</strong> on Twitter
+          <strong>{author.name}</strong> on Github
         </a>
       </p>
     </div>
