a6d3b5dd20d9770cc9ce8176c1838ffa4a5f4d56
eunsukimme <eunsu.dev@gmail.com>
2020-08-25 02:17:31 +0900

    Add Node.js CI/CD post with AWS and Docker

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,6 +122,42 @@ module.exports = {
             resolve: "gatsby-remark-responsive-iframe",
             options: { wrapperStyle: "margin-bottom: 1.0725rem" },
           },
+          // In your gatsby-config.js
+          {
+            resolve: "gatsby-transformer-remark",
+            options: {
+              plugins: [
+                {
+                  resolve: "gatsby-remark-embed-gist",
+                  options: {
+                    // Optional:
+
+                    // the github handler whose gists are to be accessed
+                    username: "eunsukimme",
+
+                    // a flag indicating whether the github default gist css should be included or not
+                    // default: true
+                    // DEPRECATED (PLEASE USE gistDefaultCssInclude)
+                    // includeDefaultCss: true || false,
+
+                    // a flag indicating whether the github default gist css should be included or not
+                    // default: true
+                    gistDefaultCssInclude: true || false,
+
+                    // a flag indicating whether the github default gist css should be preloaded or not
+                    // use this if you want to load the default css asynchronously.
+                    // default: false
+                    gistCssPreload: true || false,
+
+                    // a string that represents the github default gist css url.
+                    // defaults: "https://github.githubassets.com/assets/gist-embed-b3b573358bfc66d89e1e95dbf8319c09.css"
+                    gistCssUrlAddress:
+                      "https://github.githubassets.com/assets/gist-embed-b3b573358bfc66d89e1e95dbf8319c09.css",
+                  },
+                },
+              ],
+            },
+          },
           "gatsby-remark-autolink-headers",
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
