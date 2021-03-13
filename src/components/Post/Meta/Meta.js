b23a944fe7fa257f5e75b17a61df7c10dbefbac5
eunsukimme <eunsu.dev@gmail.com>
2021-03-13 13:07:58 +0900

    Edit Introduction-to-k8s and Add updated field on post

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -5,11 +5,14 @@ import styles from './Meta.module.scss';
 
 type Props = {
   date: string
+  updated: string
 };
 
-const Meta = ({ date }: Props) => (
+const Meta = ({ date, updated }: Props) => (
   <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {moment(date).format('D MMM YYYY')}</p>
+    <p className={styles['meta__date']}>
+      {`Published ${moment(date).format('D MMM YYYY')}`}{updated ? `| ${moment(updated).format('D MMM YYYY')}` : null}
+    </p>
   </div>
 );
 
