c5ae8525aa4e8649c5c57af9c5457736d1c87e1e
eunsukimme <eunsu.dev@gmail.com>
2021-03-13 13:17:27 +0900

    Fix syntax error

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,17 +1,18 @@
 // @flow strict
-import React from 'react';
-import moment from 'moment';
-import styles from './Meta.module.scss';
+import React from "react";
+import moment from "moment";
+import styles from "./Meta.module.scss";
 
 type Props = {
-  date: string
-  updated: string
+  date: string,
+  updated: string,
 };
 
 const Meta = ({ date, updated }: Props) => (
-  <div className={styles['meta']}>
-    <p className={styles['meta__date']}>
-      {`Published ${moment(date).format('D MMM YYYY')}`}{updated ? `| ${moment(updated).format('D MMM YYYY')}` : null}
+  <div className={styles["meta"]}>
+    <p className={styles["meta__date"]}>
+      {`Published ${moment(date).format("D MMM YYYY")}`}
+      {updated ? `| ${moment(updated).format("D MMM YYYY")}` : null}
     </p>
   </div>
 );
